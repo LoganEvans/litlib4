beefabce1db0c564ec92314230dc42ee7fd91244
Logan Evans <loganpevans@gmail.com>
2026-04-09 22:41:22 -0700

    (sig): add Duan-Ge 1979 decomposition and physical electromagnetic tensor

---
+++ Litlib/Y1979/duan1979su2/Proofs/Sorry.lean
@@ -0,0 +1,20 @@
+-- FILENAME: Litlib/Y1979/duan1979su2/Proofs/Sorry.lean
+
+import Litlib.Y1979.duan1979su2.Signature
+
+namespace Litlib.Y1979.duan1979su2.Proofs
+
+@[litlib_difficulty intractable, litlib_status Conjecture]
+instance : Eq1_13_Decomposition where
+  e := sorry
+  A := sorry
+  Gamma := sorry
+  b := sorry
+  decomposition := sorry
+
+@[litlib_difficulty intractable, litlib_status Conjecture]
+instance : Eq1_33_ElectromagneticTensor where
+  e := sorry
+  G := sorry
+  F := sorry
+  physical_tensor_def := sorry
