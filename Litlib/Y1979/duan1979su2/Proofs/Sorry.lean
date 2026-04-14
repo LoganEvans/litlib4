3565bbc8fc15748c9b5c676298cad30d274c7f96
Logan Evans <loganpevans@gmail.com>
2026-04-14 16:45:07 -0700

    (proof) Align Duan 1979 proofs with secured algebraic signature

--- Litlib/Y1979/duan1979su2/Proofs/Sorry.lean
+++ Litlib/Y1979/duan1979su2/Proofs/Sorry.lean
@@ -4,19 +4,12 @@ import Litlib.Y1979.duan1979su2.Signature
 
 namespace Litlib.Y1979.duan1979su2.Proofs
 
-@[litlib_difficulty intractable, litlib_status Conjecture]
+@[litlib_difficulty easy, litlib_status Conjecture]
 instance : Eq1_13_Decomposition where
-  e := sorry
-  A := sorry
-  Gamma := sorry
-  b := sorry
-  decomposition := sorry
+  gauge_decomposition := sorry
 
-@[litlib_difficulty intractable, litlib_status Conjecture]
+@[litlib_difficulty medium, litlib_status Conjecture]
 instance : Eq1_33_ElectromagneticTensor where
-  e := sorry
-  G := sorry
-  F := sorry
-  physical_tensor_def := sorry
+  electromagnetic_tensor := sorry
 
 end Litlib.Y1979.duan1979su2.Proofs
