eab9a285dc8eaa26ed0a9a2225919ec4dbcb2cd2
Logan Evans <loganpevans@gmail.com>
2026-04-14 12:10:14 -0700

    (proof) Align Weyl 1946 proofs with secured epsilon4 non-degeneracy condition

--- Litlib/Y1946/weyl1946classical/Proofs/Sorry.lean
+++ Litlib/Y1946/weyl1946classical/Proofs/Sorry.lean
@@ -2,26 +2,26 @@
 
 import Litlib.Y1946.weyl1946classical.Signature
 
-namespace Litlib.Y1946.weyl1946classical
+namespace Litlib.Y1946.weyl1946classical.Proofs
 
-@[litlib_difficulty easy]
+@[litlib_difficulty easy, litlib_status Conjecture]
 instance : SchursLemma where
   end_of_simple_module_is_division_ring := sorry
 
-@[litlib_difficulty easy]
+@[litlib_difficulty easy, litlib_status Conjecture]
 instance : DivRingIsSimple where
   division_ring_is_simple_ring := sorry
 
-@[litlib_difficulty easy]
+@[litlib_difficulty easy, litlib_status Conjecture]
 instance : MaschkesTheorem where
   group_ring_is_semisimple := sorry
 
-@[litlib_difficulty easy]
+@[litlib_difficulty easy, litlib_status Conjecture]
 instance : HilbertBasisTheorem where
   polynomial_is_noetherian := sorry
 
-@[litlib_difficulty hard]
+@[litlib_difficulty hard, litlib_status Conjecture]
 instance : FirstMainTheoremOrthogonalRank4 where
   unique_lorentz_invariant_rank4 := sorry
 
-end Litlib.Y1946.weyl1946classical
+end Litlib.Y1946.weyl1946classical.Proofs
