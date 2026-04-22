f0e83c6ae83bf656f4308f3abfb146086744ab26
Logan Evans <loganpevans@gmail.com>
2026-04-22 15:34:04 -0700

    (sig): Add metric non-degeneracy constraint to Capovilla 1989 CDJ theorem

--- Litlib/Y1989/capovilla1989general/Signature.lean
+++ Litlib/Y1989/capovilla1989general/Signature.lean
@@ -5,6 +5,7 @@ import Mathlib.Data.Complex.Basic
 import Mathlib.Data.Real.Basic
 import Mathlib.Data.Matrix.Basic
 import Mathlib.LinearAlgebra.Matrix.Trace
+import Mathlib.LinearAlgebra.Matrix.Determinant.Basic
 
 open BigOperators
 
@@ -47,6 +48,7 @@ class Eq6_RicciFlat
       epsilon4 α β γ δ = -epsilon4 α γ β δ ∧ 
       epsilon4 α β γ δ = -epsilon4 α β δ γ)
     (hEpsilonNondeg : epsilon4 0 1 2 3 ≠ 0)
+    (hNonDegenerate : ∀ x, Matrix.det (Matrix.of (urbantkeMetric F x)) ≠ 0)
     (hCdjConstraint : ∀ x, 
       (∑ μ : Fin 4, ∑ ν : Fin 4, ∑ ρ : Fin 4, ∑ σ : Fin 4,
         epsilon4 μ ν ρ σ * Matrix.trace (F x μ ν * F x ρ σ)) = 0) :
