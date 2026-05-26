b22b46af42e19f136c5386d31d13dac0685c5b1c
Logan Evans <loganpevans@gmail.com>
2026-05-26 04:35:50 -0700

    Roll back to Lean 4.27.0

--- lakefile.lean
+++ lakefile.lean
@@ -8,7 +8,7 @@ package "litlib4" where
   -- The litlib_run bash script handles filtering the `sorry` traces.
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.29.1"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.27.0"
 
 @[default_target]
 lean_lib «Litlib» where
