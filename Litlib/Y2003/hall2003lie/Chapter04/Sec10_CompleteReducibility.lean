12fb25757305865bb8485e9d7b56f28a768dc368
Logan Evans <loganpevans@gmail.com>
2026-08-24 19:00:02 -0700

    Litlib: Add Hall 2003 Haar measure and unimodular typeclasses

---
+++ Litlib/Y2003/hall2003lie/Chapter04/Sec10_CompleteReducibility.lean
@@ -0,0 +1,24 @@
+-- FILENAME: Litlib/Y2003/hall2003lie/Chapter04/Sec10_CompleteReducibility.lean
+
+import Litlib.Core
+import Litlib.Y2003.hall2003lie.Paper
+import Mathlib.MeasureTheory.Constructions.BorelSpace.Basic
+import Mathlib.MeasureTheory.Measure.Haar.Basic
+import Mathlib.MeasureTheory.Measure.MeasureSpace
+
+namespace Litlib.Y2003.hall2003lie
+
+Litlib.equation "hall2003lie" eq "HaarMeasure" page "120" kind "definition"
+class LeftHaarMeasure (G : Type*) [TopologicalSpace G] [Group G] [MeasurableSpace G]
+    [BorelSpace G] (μ : MeasureTheory.Measure G) : Prop where
+  is_nonzero : μ ≠ 0
+  is_locally_finite : MeasureTheory.IsLocallyFiniteMeasure μ
+  is_left_invariant : ∀ (g : G) (E : Set G), MeasurableSet E → μ ((fun x ↦ g * x) '' E) = μ E
+
+Litlib.equation "hall2003lie" eq "Unimodular" page "121" kind "definition"
+class Unimodular (G : Type*) [TopologicalSpace G] [Group G] [MeasurableSpace G]
+    [BorelSpace G] : Prop where
+  left_is_right : ∀ (μ : MeasureTheory.Measure G), LeftHaarMeasure G μ →
+    ∀ (g : G) (E : Set G), MeasurableSet E → μ ((fun x ↦ x * g) '' E) = μ E
+
+end Litlib.Y2003.hall2003lie
